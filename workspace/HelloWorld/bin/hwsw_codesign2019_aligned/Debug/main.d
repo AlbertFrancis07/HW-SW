9efadcd61eb8ba453c45008838e0012ae36ab82d
Albert Francis <albert.francis@ifn.et.tu-dresden.de>
2024-07-01 03:27:33 +0200

    latest_updated_copy

---
+++ workspace/HelloWorld/bin/hwsw_codesign2019_aligned/Debug/main.d
@@ -0,0 +1,1 @@
+main.o: U:/HWSWCODESIGN/workspace/HelloWorld/main.c
--- workspace/HelloWorld/main.c
+++ workspace/HelloWorld/main.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 
-int main( int argc, char **argv )
+void main()
 {
 	printf("Hello World\n");
-	return 0;
 }
